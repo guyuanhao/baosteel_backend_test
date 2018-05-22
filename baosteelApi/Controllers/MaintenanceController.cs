f83d07a9093b09eb0e0eaef8167b6cab609d2b30
Unknown <guyuanhao@foxmail.com>
2018-05-22 11:13:55 +0800

    add controller

---
+++ baosteelApi/Controllers/MaintenanceController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using baosteelApi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace baosteelApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class MaintenanceController : ControllerBase
+    {
+        private readonly CheckContext _context;
+
+        [HttpGet]
+        public List<MaintenanceItem> GetAll()
+        {
+            return _context.MAINTENANCE_ITEMS.ToList();
+        }
+    }
+}
