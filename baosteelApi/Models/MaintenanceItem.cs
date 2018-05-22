a73bb8debd1b8c6272da911a11c1edd4de152ff0
Unknown <guyuanhao@foxmail.com>
2018-05-22 10:24:30 +0800

    add models

--- baosteelApi/Models/MaintenanceItem.cs
+++ baosteelApi/Models/MaintenanceItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,21 @@ namespace baosteelApi.Models
 {
     public class MaintenanceItem
     {
+        [Key]
+        public int ID { get; set; }
+
+        public string PROJECT_NAME { get; set; }
+
+        public string DETAIL { get; set; }
+
+        public int PERIODE { get; set; }
+
+        public string INDICATION { get; set; }
+
+        public DateTime CREATE_DATE { get; set; }
+
+        public string RESPONSIBLE { get; set; }
+
+        public string NOTE { get; set; }
     }
 }
