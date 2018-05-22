0ad40be50e7b11e73587290b8df420e9ecd6f038
Unknown <guyuanhao@foxmail.com>
2018-05-22 10:23:47 +0800

    add models & context

---
+++ baosteelApi/Models/CheckContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace baosteelApi.Models
+{
+    public class CheckContext : DbContext
+    {
+        public CheckContext(DbContextOptions<CheckContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MaintenanceItem> MAINTENANCE_ITEMS { get; set; }
+        public DbSet<InfoItem> INFO_ITEMS { get; set; }
+    }
+}
---
+++ baosteelApi/Models/InfoItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace baosteelApi.Models
+{
+    public class InfoItem
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public string PROJECT_NAME { get; set; }
+
+        public string DETAIL { get; set; }
+
+        public string KEY_POINT { get; set; }
+
+        public string INDICATION { get; set; }
+
+        public DateTime CHECK_DATE { get; set; }
+
+        public string RESPONSIBLE { get; set; }
+
+        public Boolean IF_CHECK { get; set; }
+
+        public string NOTE { get; set; }
+    }
+}
